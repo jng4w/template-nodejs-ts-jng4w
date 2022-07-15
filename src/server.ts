82b6a0f223a4e9564652871a062d8829faa6ae16
jng4w <jng4w@github.com>
2022-07-15 15:13:05 +0700

    add cert to create https server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import app from "./app";
+import * as https from 'https';
+import * as fs from 'fs';
 
 const PORT = 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+
+const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem'),
+    cert: fs.readFileSync('./config/cert.pem')
+}
+
+https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log('Express server listening on port ' + PORT);
+})
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port: ${PORT}`);
+// });
